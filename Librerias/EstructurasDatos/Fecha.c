cb68cf0148891b5cd072f039404d4f770888a93a
nahiacfr <nahia.castaneda@opendeusto.es>
2023-05-18 16:22:11 +0200

    clase fecha

--- Librerias/EstructurasDatos/Fecha.c
+++ Librerias/EstructurasDatos/Fecha.c
@@ -13,8 +13,31 @@ Fecha* crearFecha(int dia, int mes, int anyo){
  }
 }
  
- 
+ /* Este método 
+ devuelve -1 si la fecha 1 es anterior a la fecha 2
+ devuelve 1 si la fecha 1 es posterior a la fecha 2
+ devuelve 0 si las fechas son iguales*/
 int compararFechas(Fecha fecha1, Fecha fecha2){
-
+if (fecha1.anyo < fecha2.anyo) {
+        return -1; 
+    } else if (fecha1.anyo > fecha2.anyo) {
+        return 1; 
+    } else {
+        // Los años son iguales, se compara el mes
+        if (fecha1.mes < fecha2.mes) {
+            return -1; // fecha1 es anterior a fecha2
+        } else if (fecha1.mes > fecha2.mes) {
+            return 1; // fecha1 es posterior a fecha2
+        } else {
+            // Los meses son iguales, se compara el día
+            if (fecha1.dia < fecha2.dia) {
+                return -1; // fecha1 es anterior a fecha2
+            } else if (fecha1.dia > fecha2.dia) {
+                return 1; // fecha1 es posterior a fecha2
+            } else {
+                return 0; // Las fechas son iguales
+            }
+        }
+    }
 
 }
