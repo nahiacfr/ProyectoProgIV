9803f98391cd2d8cd90a69955c7bad3624ca71fe
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2023-05-25 16:21:54 +0200

    Estructura Socket del cliente

--- mCliente.cpp
+++ mCliente.cpp
@@ -1,6 +1,9 @@
 
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <winsock2.h>
 #include <string>
 #include <windows.h> //necesario para el sleep();
 
@@ -18,6 +21,9 @@ using namespace std;
 #define MAX_LINE 50
 #define SECONDS_TO_CONTINUE 2000
 
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6000
+
 void mainMenuUser();
 void inicioSesion();
 void registraUsuario();
@@ -26,8 +32,57 @@ void buscarTitulo();
 void buscarAutor();
 void reservar(char nombre);
 
+int iResult;
+
 int main(int argc, char const *argv[])
 {
+    WSADATA wsaData;
+	SOCKET s;
+	struct sockaddr_in server;
+	char sendBuff[512], recvBuff[512];
+
+	printf("\nInitialising Winsock...\n");
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return -1;
+	}
+
+	printf("Initialised.\n");
+
+	//SOCKET creation
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		printf("Could not create socket : %d", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Socket created.\n");
+
+	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT);
+
+	//CONNECT to remote server
+	if (connect(s, (struct sockaddr*) &server, sizeof(server)) == SOCKET_ERROR) {
+		printf("Connection error: %d", WSAGetLastError());
+		closesocket(s);
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Connection stablished with: %s (%d)\n", inet_ntoa(server.sin_addr),
+			ntohs(server.sin_port));
+
+	// SEND and RECEIVE data
+	/*
+    printf("Sending message 1... \n");
+	strcpy(sendBuff, "Hello, server.");
+	send(s, sendBuff, sizeof(sendBuff), 0);
+    */
+
+	// CLOSING the socket and cleaning Winsock...
+	closesocket(s);
+	WSACleanup();
     mainMenuUser();
     return 0;
 }
